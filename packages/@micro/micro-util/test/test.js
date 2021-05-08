0b848de14a063b6a9f53262b57028584f7b402f7
hongdong.liao <hongdong.liao@vantopgroup.com>
2021-05-08 11:54:38 +0800

    feat(all): wip

---
+++ packages/@micro/micro-util/test/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: hongdong.liao
+ * @Date: 2021-05-08 11:46:25
+ * @LastEditors: hongdong.liao
+ * @LastEditTime: 2021-05-08 11:53:36
+ * @FilePath: /micro/packages/@micro/micro-util/test/111.js
+ */
+import { Helper } from '../lib';
+console.log(Helper);
